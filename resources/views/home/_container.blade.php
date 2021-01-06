b8505fd0df36b9ab85aa7212a9c78123007b9f67
erkanbeskardess <erkan5kardes@gmail.com>
2021-01-06 16:52:13 +0300

    ek dosyalar eklendi.

---
+++ resources/views/home/_container.blade.php
@@ -0,0 +1,39 @@
+<div class="cat-main-box">
+    <div class="container">
+        <div class="row panel-row">
+            <div class="col-md-4 col-sm-6 wow fadeIn" data-wow-delay="0.0s">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <img src="{{asset('assets')}}/images/xpann-icon.jpg" class="icon-small" alt="">
+                        <h4>“Chamb” Your Business</h4>
+                        <p>Grow easily with chamb. Create free account.
+                            We help expanding your business easily.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 wow fadeIn" data-wow-delay="0.2s">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <img src="{{asset('assets')}}/images/create-icon.jpg" class="icon-small" alt="">
+                        <h4>Create and add</h4>
+                        <p>Grow easily with chamb. Create free account.
+                            We help expanding your business easily.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 wow fadeIn hidden-sm" data-wow-delay="0.4s">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <img src="{{asset('assets')}}/images/get-icon.jpg" class="icon-small" alt="">
+                        <h4>Get inspired</h4>
+                        <p>Grow easily with chamb. Create free account.
+                            We help expanding your business easily.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
