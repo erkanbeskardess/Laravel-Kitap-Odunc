353bd64a999c6b96f2a557c9a4c2219d9a3179b1
erkanbeskardess <erkan5kardes@gmail.com>
2021-01-06 16:45:27 +0300

    template uyarlandı

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,30 +1,264 @@
 @extends('layouts.main')
-
-@section('title','Sub Page Title')
-@section('sidebar')
-
-    <div class="col-sm-4">
-        <h1>Kategori</h1>
-        <ul>
-            <li>Roman</li>
-            <li>Hikaye</li>
-
-            <li>Masal</li>
-            <li>Biyografi</li>
-            <li>sa</li>
-
-
-
-        </ul>
-    </div>
-@endsection
-
+@section('title','KtpOdunc')
 @section('content')
-    <div class="col-sm-8">
-        <h3>Slider</h3>
-        <br>
 
-        <br>
+    <div class="products_exciting_box">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 wow fadeIn" data-wow-delay="0.2s">
+                    <div class="exciting_box f_pd">
+                        <img src="{{asset('assets')}}/images/exciting_img-01.jpg" class="icon-small" alt="" />
+                        <h4>Explore <strong>exciting</strong> and exotic products
+                            tailored to you.
+                        </h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris..
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 wow fadeIn" data-wow-delay="0.4s">
+                    <div class="exciting_box l_pd">
+                        <img src="{{asset('assets')}}/images/exciting_img-02.jpg" class="icon-small" alt="" />
+                        <h4><strong>List your products on</strong> chamb and grow connections.</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris..
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-@endsection
+    <div class="start-free-box">
+        <div class="container">
+            <div class="row">
+                <div class="container">
+                    <div class="main-start-box">
+                        <div class="free-box-a clearfix">
+                            <div class="col-md-6 col-sm-6">
+                                <div class="left-a-f">
+                                    <h3>A platform built for scale & expansion. Start for free.</h3>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <div class="right-a-f">
+                                    <p>Over the comming years, way thet business through the web
+                                        works will change at agreat - and chamb is the
+                                        gamebreaker.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="main-start-box">
+                    <div class="bg_img_left"><img src="{{asset('assets')}}/images/bg_img1.png" alt="#" /></div>
+                    <div class="container">
+                        <div class="buyer-box clearfix">
+                            <div class="col-md-6 col-sm-6 wow fadeIn" data-wow-delay="0.2s">
+                                <div class="left-buyer">
+                                    <img class="img-responsive" src="{{asset('assets')}}/images/creat_pro.png" alt="#" />
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-6 wow fadeIn" data-wow-delay="0.4s">
+                                <div class="right-buyer">
+                                    <h4>buyer</h4>
+                                    <h2>Empower your factory<br>
+                                        <span>With a new lead Channel</span>
+                                    </h2>
+                                    <p>Never worry about sales or income ftom outbound
+                                        channels. with chamb your store is directly
+                                        connected to thousands of interested in your
+                                        products.
+                                    </p>
+                                    <a href="#">Create a buyer account</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="bg_img_right"><img src="{{asset('assets')}}/images/bg_img1.png" alt="#" /></div>
+                <div class="main-start-box">
+                    <div class="container">
+                        <div class="supplier clearfix">
+                            <div class="col-md-5 col-sm-6">
+                                <div class="left-supplier">
+                                    <h4>supplier</h4>
+                                    <h2>Grow your store <br><span>With a new sales channel</span></h2>
+                                    <p>Never worry about sales or income ftom outbound
+                                        channels. with chamb your store is directly
+                                        connected to thousands of interested in your
+                                        products.
+                                    </p>
+                                    .
+                                    <a href="#">Create a supplier account</a>
+                                </div>
+                            </div>
+                            <div class="col-md-7 col-sm-6">
+                                <div class="right-supplier">
+                                    <img class="img-responsive" src="{{asset('assets')}}/images/supplier-pc-img.png" alt="#" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="products">
+        <div class="main-products">
+            <h2>TRENDING PRODUCTS ON CHAMB</h2>
+            <div class="product-slidr">
+                <div class="slider">
+                    <div>
+                        <div class="prod-box">
+                            <div class="prod-i">
+                                <img src="{{asset('assets')}}/images/tr1.png" alt="#" />
+                            </div>
+                            <div class="prod-dit clearfix">
+                                <div class="dit-t clearfix">
+                                    <div class="left-ti">
+                                        <h4>Table with Lights</h4>
+                                        <p>By <span>Beko</span> under <span>Lights</span></p>
+                                    </div>
+                                    <a href="#">$1220</a>
+                                </div>
+                                <div class="dit-btn clearfix">
+                                    <a class="wis" href="#"><i class="fa fa-star" aria-hidden="true"></i> Save to wishlist </a>
+                                    <a class="thi" href="#"><i class="fa fa-thumbs-up" aria-hidden="true"></i> Like this</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="prod-box">
+                            <div class="prod-i">
+                                <img src="{{asset('assets')}}/images/tr2.png" alt="#" />
+                            </div>
+                            <div class="prod-dit clearfix">
+                                <div class="dit-t clearfix">
+                                    <div class="left-ti">
+                                        <h4>Treehouse Bed</h4>
+                                        <p>By <span>Beko</span> under <span>Lights</span></p>
+                                    </div>
+                                    <a href="#">$1220</a>
+                                </div>
+                                <div class="dit-btn clearfix">
+                                    <a class="wis" href="#"><i class="fa fa-star" aria-hidden="true"></i> Save to wishlist </a>
+                                    <a class="thi" href="#"><i class="fa fa-thumbs-up" aria-hidden="true"></i> Like this</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="prod-box">
+                            <div class="prod-i">
+                                <img src="{{asset('assets')}}/images/tr3.png" alt="#" />
+                            </div>
+                            <div class="prod-dit clearfix">
+                                <div class="dit-t clearfix">
+                                    <div class="left-ti">
+                                        <h4>Wood Sofaplatform</h4>
+                                        <p>By <span>Beko</span> under <span>Lights</span></p>
+                                    </div>
+                                    <a href="#">$1220</a>
+                                </div>
+                                <div class="dit-btn clearfix">
+                                    <a class="wis" href="#"><i class="fa fa-star" aria-hidden="true"></i> Save to wishlist </a>
+                                    <a class="thi" href="#"><i class="fa fa-thumbs-up" aria-hidden="true"></i> Like this</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="prod-box">
+                            <div class="prod-i">
+                                <img src="{{asset('assets')}}/images/tr4.png" alt="#" />
+                            </div>
+                            <div class="prod-dit clearfix">
+                                <div class="dit-t clearfix">
+                                    <div class="left-ti">
+                                        <h4>Wall Sticker</h4>
+                                        <p>By <span>Beko</span> under <span>Lights</span></p>
+                                    </div>
+                                    <a href="#">$1220</a>
+                                </div>
+                                <div class="dit-btn clearfix">
+                                    <a class="wis" href="#"><i class="fa fa-star" aria-hidden="true"></i> Save to wishlist </a>
+                                    <a class="thi" href="#"><i class="fa fa-thumbs-up" aria-hidden="true"></i> Like this</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="prod-box">
+                            <div class="prod-i">
+                                <img src="{{asset('assets')}}/images/tr1.png" alt="#" />
+                            </div>
+                            <div class="prod-dit clearfix">
+                                <div class="dit-t clearfix">
+                                    <div class="left-ti">
+                                        <h4>Treehouse Bed</h4>
+                                        <p>By <span>Beko</span> under <span>Lights</span></p>
+                                    </div>
+                                    <a href="#">$1220</a>
+                                </div>
+                                <div class="dit-btn clearfix">
+                                    <a class="wis" href="#"><i class="fa fa-star" aria-hidden="true"></i> Save to wishlist </a>
+                                    <a class="thi" href="#"><i class="fa fa-thumbs-up" aria-hidden="true"></i> Like this</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="prod-box">
+                            <div class="prod-i">
+                                <img src="{{asset('assets')}}/images/tr2.png" alt="#" />
+                            </div>
+                            <div class="prod-dit clearfix">
+                                <div class="dit-t clearfix">
+                                    <div class="left-ti">
+                                        <h4>Treehouse Bed</h4>
+                                        <p>By <span>Beko</span> under <span>Lights</span></p>
+                                    </div>
+                                    <a href="#">$1220</a>
+                                </div>
+                                <div class="dit-btn clearfix">
+                                    <a class="wis" href="#"><i class="fa fa-star" aria-hidden="true"></i> Save to wishlist </a>
+                                    <a class="thi" href="#"><i class="fa fa-thumbs-up" aria-hidden="true"></i> Like this</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="prod-box">
+                            <div class="prod-i">
+                                <img src="{{asset('assets')}}/images/tr3.png" alt="#" />
+                            </div>
+                            <div class="prod-dit clearfix">
+                                <div class="dit-t clearfix">
+                                    <div class="left-ti">
+                                        <h4>Treehouse Bed</h4>
+                                        <p>By <span>Beko</span> under <span>Lights</span></p>
+                                    </div>
+                                    <a href="#">$1220</a>
+                                </div>
+                                <div class="dit-btn clearfix">
+                                    <a class="wis" href="#"><i class="fa fa-star" aria-hidden="true"></i> Save to wishlist </a>
+                                    <a class="thi" href="#"><i class="fa fa-thumbs-up" aria-hidden="true"></i> Like this</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,37 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
+    <meta charset="UTF-8">
     <title>@yield('title')</title>
-    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--enable mobile device-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    @yield('css')
-    @yield('js')
+    <!--fontawesome css-->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/font-awesome.min.css">
+    <!--bootstrap css-->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap.min.css">
+    <!--animate css-->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/animate-wow.css">
+    <!--main css-->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/style.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/slick.min.css">
+    <!--responsive css-->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/responsive.css">
 </head>
 <body>
-<div class="jumbotron text-center">
-    <h1>My First Bootstrap Page</h1>
-    <a href="/home">Anasayfa</a>
-    <a href="/aboutus">About us</a>
-    @yield('header')
-
-</div>
-
+@include('home._header')
+@include('home._navi')
+@include('home._container')
+@section('content')
+    @show
+@include('home._footer')
 
 
-<div class="container">
-    <div class="row">
-        @yield('sidebar')
-        @yield('content')
-    </div>
-</div>
-<div class="jumbotron text-center">
-    <h1>Footer</h1>
-    <p>Sayfa altı</p>
-    @yield('footer')
-   </div>
+
+
+
+
+
+
+
 
 </body>
 </html>
